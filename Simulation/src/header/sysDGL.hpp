a915e6a7bc2fea5ecf3d47ba039f19cd9471c2fe
unb3rechenbar <tom.folgmann@icloud.com>
2023-10-20 23:30:50 +0200

    V Dimensionsfehler

--- Simulation/src/header/sysDGL.hpp
+++ Simulation/src/header/sysDGL.hpp
@@ -1,25 +1,19 @@
-
-/*
-double kinE(double t, Lsng* L) {
-    return scpr(L->p,L->p) / (2 * mS);
+double dVsum(Lsng* L) {
+    double sum = 0;
+    sum += mE * 2 / (pow(pnorm(2, locE - L->q1), 3)) * pnorm(1, locE - L->q1);
+    sum += mM * 2 / (pow(pnorm(2, locM - L->q1), 3)) * pnorm(1, locM - L->q1);
 }
-
-
-double potE(double t, Lsng* L) {
-    return Gravk * (mS * mE / pnorm(2, L->q - locE) + mS * mM / pnorm(2, L->q - locM));
-}
-*/
 
 
 void F(double t, Lsng* L, int k) {
     switch (k) {
         case 1:
             L->dp1 = (L->p1) * (-1) / mS;
-            // L->dq1 = // ......... d/dq V(t, q);
+            L->dq1 = dVsum(L) * Gravk * mS; // ......... d/dq V(t, q);
             break;
         case 2:
             L->dp2 = (L->p2) * (-1) / mS;
-            // L->dq2 = // ......... d/dq V(t, q);
+            L->dq2 = dVsum(L) * Gravk * mS; // ......... d/dq V(t, q);
             break;
         default:
             break;
